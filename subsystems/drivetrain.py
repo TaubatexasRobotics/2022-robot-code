ed7c1516b35165c65d83940747baaf5fa2bccd41
Victor <victorssantos572@gmail.com>
2022-02-17 21:22:32 -0300

    commented code #2

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -1,13 +1,16 @@
+# Importing libraries and constant values
 import ctre
-import constants
+import constants # Values from constants.py
 import commands2
 import wpilib
 import wpilib.drive
 
+# Drivetrain subsystem class
 class Drivetrain(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
         
+        # Controls for Drivetrain system
         self.m_left_front = ctre.WPI_VictorSPX(constants.C_M_LEFT_FRONT)
         self.m_right_front = ctre.WPI_VictorSPX(constants.C_M_RIGHT_FRONT)
         self.m_left_back = ctre.WPI_VictorSPX(constants.C_M_LEFT_BACK)
@@ -19,5 +22,6 @@ class Drivetrain(commands2.SubsystemBase):
         self.robot_drive = wpilib.drive.DifferentialDrive(self.m_left, self.m_right)
         self.robot_drive.setExpiration(0.1)
    	
+    # Function for start motors (?)
     def setMotors(self, x_speed : float, z_rotation : float) -> None:
         self.robot_drive.arcadeDrive(x_speed, z_rotation, True)
