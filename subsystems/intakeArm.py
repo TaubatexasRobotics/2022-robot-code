39bb7afc12b43c6521bf8b5e7e206e2d8decfd49
Anna <ab.calderaro@outlook.com>
2022-03-05 16:48:45 -0300

    intake

--- subsystems/intakeArm.py
+++ subsystems/intakeArm.py
@@ -9,15 +9,10 @@ class IntakeArm(commands2.SubsystemBase):
         self.m_intakeArm = ctre.WPI_VictorSPX(constants.C_M_ARM_INTAKE)
 
     def intake_move_up(self):
-
         self.m_intakeArm.set(1)
-        print("sobe")
 
     def intake_move_down(self):
-
         self.m_intakeArm.set(-1)
-        print("desce")
     
     def intake_move_stop(self):
         self.m_intakeArm.set(0)
-        print("para")
--- subsystems/intakeRolamento.py
+++ subsystems/intakeRolamento.py
@@ -10,16 +10,10 @@ class IntakeRolamento(commands2.SubsystemBase):
         self.m_intakeRolamento = ctre.WPI_VictorSPX(constants.C_M_ROLAMENTO_INTAKE)
 
     def intake_rolamento_pull(self):
-
         self.m_intakeRolamento.set(1)
-        print("sobeRolamento")
 
     def intake_rolamento_push(self):
-
         self.m_intakeRolamento.set(-1)
-        print("desceRolamento")
 
     def intake_rolamento_stop(self):
-
         self.m_intakeRolamento.set(0)
-        print("paraRolamento")
