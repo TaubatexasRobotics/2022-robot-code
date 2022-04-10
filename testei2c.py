f7f1300291cf4fd71ac3dc634a524c294ceed0c7
maria clara couto lorena <mariaclaracoutolorena@gmail.com>
2022-04-10 08:36:09 -0300

    atualização da função i2c, posterior commit adicionando no autonomo

--- testei2c.py
+++ testei2c.py
@@ -3,6 +3,12 @@ slave_address = 4
 Wire = wpilib.I2C(wpilib._wpilib.I2C.Port.kOnboard,slave_address)
 MAX_BYTES = 32
 def getData():
+    #onde será armazenada a informação recebida
     data = []
-    Wire.read(slave_address, MAX_BYTES, data)
-    return data
+
+    #
+    pegou = Wire.read(slave_address, MAX_BYTES, data)
+
+    if pegou == True:
+        print("informação recebida")
+        print(data)
