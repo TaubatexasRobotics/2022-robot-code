62e5293882f3b050b6cd031f05b03f3cdd08266a
Guilherme Samuel <guisamuel800@gmail.com>
2022-06-26 17:54:33 -0300

    feat: controllers classes with all commands

--- controllers/base_controller.py
+++ controllers/base_controller.py
@@ -8,6 +8,10 @@ class BaseController(ABC):
         self.__outtakeCommand = None
         self.__armUpCommand = None
         self.__armDownCommand = None
+        self.__extendClimberCommand = None
+        self.__contractClimberCommand = None
+        self.__climberAngleForwardCommand = None
+        self.__climberAngleBackwardCommand = None
 
     # Getters (Stick Axis)
     @abstractmethod
@@ -25,6 +29,15 @@ class BaseController(ABC):
     @abstractmethod
     def getRightStickY(self) -> float:
         pass
+    
+    # Get Triggers (Axis)
+    @abstractmethod
+    def getLeftTrigger(self) -> float:
+        pass
+
+    @abstractmethod
+    def getRightTrigger(self) -> float:
+        pass    
 
     # Getters (POVs)
     @abstractmethod
@@ -60,11 +73,88 @@ class BaseController(ABC):
     def armDownCommand(self) -> CommandBase:
         return self.__armDownCommand
     
-    # Setters (Commands)
-    @abstractmethod
-    def setupIntakeCommand(self, newIntakeCommand : CommandBase):
-        pass
+    @property
+    def extendClimberCommand(self) -> CommandBase:
+        return self.__extendClimberCommand
+    
+    @property
+    def extendClimberCommand(self) -> CommandBase:
+        return self.__extendClimberCommand
 
+    @property
+    def contractClimberCommand(self) -> CommandBase:
+        return self.__contractClimberCommand
+
+    @property
+    def climberAngleForwardCommand(self) -> CommandBase:
+        return self.__climberAngleForwardCommand
+
+    @property
+    def climberAngleBackwardCommand(self) -> CommandBase:
+        return self.__climberAngleBackwardCommand
+
+    # Setters (Commands)
     @intakeCommand.setter
     def intakeCommand(self, newIntakeCommand : CommandBase) -> None:
         self.__intakeCommand = newIntakeCommand
+    
+    @outtakeCommand.setter
+    def outtakeCommand(self, newOuttakeCommand : CommandBase) -> None:
+        self.__outtakeCommand = newOuttakeCommand
+    
+    @armUpCommand.setter
+    def armUpCommand(self, newArmUpCommand : CommandBase) -> None:
+        self.__armUpCommand = newArmUpCommand
+
+    @armDownCommand.setter
+    def armDownCommand(self, newArmDownCommand : CommandBase) -> None:
+        self.__armDownCommand = newArmDownCommand
+
+    @extendClimberCommand.setter
+    def extendClimberCommand(self, newExtendClimberCommand : CommandBase) -> None:
+        self.__extendClimberCommand = newExtendClimberCommand
+
+    @contractClimberCommand.setter
+    def contractClimberCommand(self, newContractClimberCommand : CommandBase) -> None:
+        self.__contractClimberCommand = newContractClimberCommand
+
+    @climberAngleForwardCommand.setter
+    def climberAngleForwardCommand(self, newClimberAngleForwardCommand : CommandBase) -> None:
+        self.__climberAngleForwardCommand = newClimberAngleForwardCommand
+
+    @climberAngleBackwardCommand.setter
+    def climberAngleBackwardCommand(self, newClimberAngleBackwardCommand : CommandBase) -> None:
+        self.__climberAngleBackwardCommand = newClimberAngleBackwardCommand
+
+    # Setup Commands
+    @abstractmethod
+    def setupIntakeCommand(self, newIntakeCommand : CommandBase) -> None:
+        pass
+    
+    @abstractmethod
+    def setupOuttakeCommand(self, newOuttakeCommand : CommandBase) -> None:
+        pass
+    
+    @abstractmethod
+    def setupArmUpCommand(self, newArmUpCommand : CommandBase) -> None:
+        pass
+    
+    @abstractmethod
+    def setupArmDownCommand(self, newArmDownCommand : CommandBase) -> None:
+        pass
+    
+    @abstractmethod
+    def setupExtendClimberCommand(self, newExtendClimberCommand : CommandBase) -> None:
+        pass
+    
+    @abstractmethod
+    def setupContractClimberCommand(self, newContractClimberCommand : CommandBase) -> None:
+        pass
+
+    @abstractmethod
+    def setupClimberAngleForwardCommand(self, newClimberAngleForwardCommand : CommandBase) -> None:
+        pass
+    
+    @abstractmethod
+    def setupClimberAngleBackwardCommand(self, newClimberAngleBackwardCommand : CommandBase) -> None:
+        pass
--- controllers/generic_xbox_controller.py
+++ controllers/generic_xbox_controller.py
@@ -5,13 +5,12 @@ from commands2.button import JoystickButton, POVButton
 from .buttons import g_xbox_360
 
 class GenericXboxController(BaseController):
-    
     # Constructor
     def __init__(self, port : int) -> None:
         super().__init__() # Call BaseController Constructor
         self.controller = GenericHID(port)
     
-    # Getters (Sticks)
+    # Getters (Stick Axis)
     def getLeftStickX(self) -> float:
         return self.controller.getRawAxis(0)
     
@@ -23,6 +22,15 @@ class GenericXboxController(BaseController):
 
     def getRightStickY(self) -> float:
         return self.controller.getRawAxis(5)
+
+    # Get Triggers (Axis)
+    @abstractmethod
+    def getLeftTrigger(self) -> float:
+        return self.controller.getRawAxis(2)
+
+    @abstractmethod
+    def getRightTrigger(self) -> float:
+        return self.controller.getRawAxis(3)
 
     # Getters (POVs)
     def getPOVUp(self) -> float:
@@ -40,3 +48,24 @@ class GenericXboxController(BaseController):
     # Setup Commands
     def setupIntakeCommand(self, newIntakeCommand : CommandBase) -> None:
         JoystickButton(self.controller, g_xbox_360['lb']).whenHeld(newIntakeCommand)
+    
+    def setupOuttakeCommand(self, newOuttakeCommand : CommandBase) -> None:
+        JoystickButton(self.controller, g_xbox_360['rb']).whenHeld(newOuttakeCommand)
+    
+    def setupArmUpCommand(self, newArmUpCommand : CommandBase) -> None:
+        JoystickButton(self.controller, g_xbox_360['a']).whenHeld(newArmUpCommand)
+    
+    def setupArmDownCommand(self, newArmDownCommand : CommandBase) -> None:
+        JoystickButton(self.controller, g_xbox_360['b']).whenHeld(newArmDownCommand)
+    
+    def setupExtendClimberCommand(self, newExtendClimberCommand : CommandBase) -> None:
+        JoystickButton(self.controller, self.getPOVUp()).whenHeld(newExtendClimberCommand)
+    
+    def setupContractClimberCommand(self, newContractClimberCommand : CommandBase) -> None:
+        JoystickButton(self.controller, self.getPOVDown()).whenHeld(newContractClimberCommand)
+
+    def setupClimberAngleForwardCommand(self, newClimberAngleForwardCommand : CommandBase) -> None:
+        JoystickButton(self.controller, self.getPOVRight()).whenHeld(newClimberAngleForwardCommand)
+    
+    def setupClimberAngleBackwardCommand(self, newClimberAngleBackwardCommand : CommandBase) -> None:
+        JoystickButton(self.controller, self.getPOVLeft()).whenHeld(newClimberAngleBackwardCommand)
