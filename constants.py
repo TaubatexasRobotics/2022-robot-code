a97dc79b9787ee93f3174e5068db4b0d4d0a7879
Guilherme Samuel <guisamuel800@gmail.com>
2022-06-27 23:12:12 -0300

    feat: LEFT and RIGHT constants for climber angle

--- constants.py
+++ constants.py
@@ -11,7 +11,8 @@ C_M_RIGHT_BACK = 4
 # Climber motor controllers
 C_M_LEFT_HOOK = 5
 C_M_RIGHT_HOOK = 6
-C_M_ANGLE_HOOK = 9
+C_M_LEFT_ANGLE_HOOK = 9
+C_M_RIGHT_ANGLE_HOOK = 10
 
 # Intake controller
 C_M_INTAKE = 7
@@ -19,7 +20,7 @@ C_M_INTAKE = 7
 # Arm Controller
 C_M_ARM = 8
 
-#### ARM TIMER CONSTANTS ####
+##### ARM TIMER CONSTANTS #####
 C_ARM_HOLD_UP = 0.08
 C_ARM_HOLD_DOWN = 0.13
 C_ARM_TRAVEL = 0.5
@@ -32,7 +33,7 @@ C_ARM_TIME_DOWN = 0.35
 C_DRIVER_CONTROLLER = 0
 
 # Drivetrain parameters
-C_BUFFER_Z_ROTATION = 1.15
+C_BUFFER_Z_ROTATION = 1
 C_BUFFER_X_SPEED = 1
 
 # Serial Connection
