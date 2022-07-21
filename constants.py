fbe8ce2b01f216bd7a284f0adcce7ba5c831eb12
Guilherme Samuel <guisamuel800@gmail.com>
2022-07-21 00:58:08 -0300

    feat: usb constants and conveyor

--- constants.py
+++ constants.py
@@ -2,30 +2,23 @@
 
 ##### CONTROLLERS #####
 
-# DriveTrain Controllers
+# Drivetrain Motor Controllers
 C_M_LEFT_BACK = 1
 C_M_LEFT_FRONT = 2
 C_M_RIGHT_FRONT = 3
 C_M_RIGHT_BACK = 4
 
-# Climber motor controllers
+# Climber Motor Controllers
 C_M_LEFT_HOOK = 5
 C_M_RIGHT_HOOK = 6
 C_M_LEFT_ANGLE_HOOK = 9
 C_M_RIGHT_ANGLE_HOOK = 10
 
-# Intake controller
+# Intake Motor Controller
 C_M_INTAKE = 7
 
-# Arm Controller
-C_M_ARM = 8
-
-##### ARM TIMER CONSTANTS #####
-C_ARM_HOLD_UP = 0.08
-C_ARM_HOLD_DOWN = 0.13
-C_ARM_TRAVEL = 0.5
-C_ARM_TIME_UP = 0.5
-C_ARM_TIME_DOWN = 0.35
+# Conveyor Motor Controller
+C_M_CONVEYOR = 8
 
 ##### OTHER CONSTANTS AND PARAMETERS #####
 
@@ -38,3 +31,8 @@ C_BUFFER_X_SPEED = 1
 
 # Serial Connection
 C_BAUD_RATE = 9600
+
+# roboRIO USBs ports
+C_ROBORIO_USB = 2 # wpilib.SerialPort.Port.kUSB
+C_ROBORIO_USB_1 = 2  # wpilib.SerialPort.Port.kUSB1
+C_ROBORIO_USB_2 = 3 # wpilib.SerialPort.Port.kUSB2
