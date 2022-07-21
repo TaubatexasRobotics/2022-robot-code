884a06de3cac359fac03924992111920d42a96a1
Guilherme Samuel <guisamuel800@gmail.com>
2022-07-21 00:19:51 -0300

    refactor: update pins + comments

--- serial/ultrassonic_sensor/ultrassonic_sensor.ino
+++ serial/ultrassonic_sensor/ultrassonic_sensor.ino
@@ -1,31 +1,32 @@
+// Attach pins on Arduino
+#define echoPin 8
+#define trigPin 7
 
-#define echoPin 8 
-#define trigPin 7 
-
-// defines variables
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-int media;
+long duration; // Duration of sound wave travel
+int distance; // Distance measurement
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-
 }
 
 void loop() {
-  
+  // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   
+  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-
-  distance = duration * 0.034 / 2; 
   
-  Serial.println(distance);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  
+  // Displays the distance on the Serial Port
+  Serial.print(distance);
 }
